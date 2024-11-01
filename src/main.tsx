cafc533281c98f0e679b0ddfe75f18bcbe86596e
Klomachenko <leekyumin0901@naver.com>
2024-11-02 04:10:27 +0900

    test: msw 설정 후 요청 test Description: 요청 성공

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,24 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './reset.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+async function enableMocking() {
+  if (import.meta.env.VITE_REACT_APP_NODE_ENV !== 'development') {
+    return;
+  }
+
+  const { worker } = await import('./mocks/browser.ts');
+  return worker.start();
+}
+
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  throw new Error('Root element not found');
+}
+
+enableMocking().then(() => {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+});
